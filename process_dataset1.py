04151f7f280b7ab50d9a8d3df893cd0b76c47a17
sunjiaqian1218@163.com <sunjiaqian1218@163.com>
2019-11-09 16:32:15 -0500

    drop NAN value, change the absolute path to relative path

--- process_dataset1.py
+++ process_dataset1.py
@@ -4,8 +4,13 @@ Created on Sat Nov  9 14:32:16 2019
 
 @author: seanl
 """
-dataset1_file = "/Users/seanl/Documents/506/final_project/6ddcd912-32a0-43df-9908-63574f8c7e77.csv"
-dataset2_file = "/Users/seanl/Documents/506/final_project/fy19fullpropassess.csv"
+
+import os
+
+abslote_path = os.path.abspath('.')
+
+dataset1_file = abslote_path + "/6ddcd912-32a0-43df-9908-63574f8c7e77.csv"
+dataset2_file = abslote_path + "/ast2018full.csv"
 
 headname =['permitnumber','comments','issued_date','zip',
            'property_id','parcel_id','lat','long']
@@ -17,12 +22,18 @@ from pandas import DataFrame
 
 def process_intervention_data_from_file(dataset_file):
     raw_data = pd.read_csv(dataset_file)[headname]
-    #fill null
+    # fill null
     raw_data['comments']=raw_data['comments'].fillna('null')
+
+    # display all of the column value
+    pd.set_option('display.max_columns', None)
     
     #select intervention related sample 
     df = pd.DataFrame(raw_data)
-    df = df[df['comments'].str.contains(keyword_test) ]
-    print(df)    
+    df = df[df['comments'].str.contains(keyword_test)]
+
+    # drop all the NAN longitude or latitude
+    df.dropna(inplace=True)
+    print(df)
     
 process_intervention_data_from_file(dataset1_file)
